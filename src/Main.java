1bbe82490327fc009b98c04834d1ad37a8557766
Parmeet Gill <parmeetgill422@gmail.com>
2025-04-21 14:16:34 -0700

    cc

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] quotes = {
+                "“Be yourself; everyone else is already taken.” – Oscar Wilde",
+                "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” – Albert Einstein",
+                "“So many books, so little time.” – Frank Zappa",
+                "“A room without books is like a body without a soul.” – Cicero",
+                "“In three words I can sum up everything I've learned about life: it goes on.” – Robert Frost",
+                "“If you tell the truth, you don't have to remember anything.” – Mark Twain",
+                "“Always forgive your enemies; nothing annoys them so much.” – Oscar Wilde",
+                "“Without music, life would be a mistake.” – Friedrich Nietzsche",
+                "“We accept the love we think we deserve.” – Stephen Chbosky",
+                "“To live is the rarest thing in the world. Most people exist, that is all.” – Oscar Wilde"
+        };
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean again = true;
+
+        while (again) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1–10: Show that quote");
+            System.out.println("R  : Show a random quote");
+            System.out.print("Your choice: ");
+            String choice = scanner.nextLine().toUpperCase();
+
+            if (choice.equals("R")) {
+                int idx = random.nextInt(quotes.length);
+                System.out.println("\n" + quotes[idx]);
+            } else { int num = Integer.parseInt(choice);
+                    System.out.println("\n" + quotes[num - 1]);
+
+            }
+
+            System.out.print("\nSee another quote? (Y/N): ");
+            String resp = scanner.nextLine().toUpperCase();
+            if (!resp.equals("Y")) {
+                again = false;
+            }
         }
+
+        System.out.println("\nThanks for using my FamousQuotes. Now go away!");
+
     }
 }
